6d83d8bd92cafa8af4627622f946503e8830bc9a
Hiren Asodariya <asodariya.hiren33@gmail.com>
2026-07-13 15:30:50 +0530

    changes for fixture file

---
+++ fixture.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+function isSorted(arr: number[]): boolean {
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < arr[i - 1])
+            return false;
+    }
+    return true;
+}
