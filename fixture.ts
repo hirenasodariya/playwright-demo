e39aa0f51104cfb2eca5d171cb92a926decfebef
Hiren Asodariya <asodariya.hiren33@gmail.com>
2026-07-14 13:03:36 +0530

    updated method name

--- fixture.ts
+++ fixture.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-function isAccending(arr: number[]): boolean {
+function isAsceding(arr: number[]): boolean {
     for (let i = 1; i < arr.length; i++) {
         if (arr[i] < arr[i - 1])
             return false;
